5415be8b4ee078cc345d743bc366cf9f9f226e6b
Eddy Meals <edimea211@gmail.com>
2024-03-03 18:34:32 -0800

    Add .gitignore and challenges

---
+++ challenges.py
@@ -0,0 +1,103 @@
+def delivery(lst):
+    meal_counts = {
+        "Ordinary Prisoner": 1,
+        "Pugilist Prisoner": 2,
+        "Herculean Prisoner": 3,
+        "Recidivist": 4,
+        "Infamous Recidivist": 5
+    }
+    return sum(meal_counts[prisoner] for prisoner in lst) + 1 # add one for us
+
+def prisoners_list(lst):
+    return list(set(lst))
+
+def check_for_contraband(belongings):
+    blocklist = {"knife", "drugs", "weapons", "cellphone", "alcohol"}
+    contraband = [item for item in belongings if item in blocklist]
+    return (not contraband, contraband)
+
+def hop_hop(n):
+    # n == 1 also being a base case saves us from checking whether hop_hop(n - 2) is valid
+    return 1 if n < 2 else hop_hop(n - 1) + hop_hop(n - 2)
+
+def survival_points(dict1, dict2):
+    # a list constructed from a dictionary contains keys
+    # could be made more efficient by removing duplicates from list(dict1) + list(dict2)
+    return {x: dict1.get(x, 0) + dict2.get(x, 0) for x in list(dict1) + list(dict2)}
+
+# PiE's setup code for add_visitor:
+visitation_slots = [
+    ("09:00", "10:00"),
+    ("10:00", "11:00"),
+    ("11:00", "12:00"),
+    ("13:00", "14:00"),
+    ("14:00", "15:00")]
+visitors = {slot: None for slot in visitation_slots}
+def display_schedule():
+    for slot, visitor in visitors.items():
+        print(f"{slot}: {visitor or 'Available'}")
+# end setup
+
+def add_visitor(slot, visitor_name):
+    visitors[slot] = visitors[slot] or visitor_name
+    display_schedule()
+
+# setup for acquaintance
+def count_links(id1, id2, known, visited):
+    if not id2 in known[id1]:
+        links = 0
+        for id3 in known[id1]:
+            if not id3 in visited:
+                links += count_links(id3, id2, known, visited + [id3])
+        if links >= 3:
+            # cache the indirect connection between id1 and id2
+            known[id1].add(id2)
+    return 1 if id2 in known[id1] else 0
+# end setup
+
+def acquaintance(id1, id2, *lst):
+    # TODO: 100% sure there is a solution with better running time because known is a graph and
+    # there a zillion better ways to walk graphs than this
+
+    # initially only contains direct neighbors, but will be updated with indirect neighbors
+    # (connected by three or more nodes) to make repeated traversals of the same path faster:
+    known = {}
+    for group in lst:
+        for member in group:
+            # finding neighbors this way causes all(x in known[x] for x in known) but that won't
+            # cause issues for us
+            known.setdefault(member, set()).update(group)
+    # consider id1 already visited to prevent first invocation calling count_links(id1, id2, [id1])
+    # which would cause the set to be modified while the first invocation is still looping over it
+    return bool(count_links(id1, id2, known, [id1]))
+
+def ssspookyyyy(str):
+    # TODO: figure out the weird group theory mathy solution that probably exists
+    # also this probably isn't optimal (we wastefully find parts every recursion) but hopefully
+    # still passes test cases
+    if not str:
+        return []
+    # process string into array of [character, run_length]
+    parts = []
+    for c in str:
+        if not parts or parts[-1][0] != c:
+            parts.append([c, 1])
+        else:
+            parts[-1][1] += 1
+    # find prospective beginnings and ends of longest chunk
+    begs = [0]
+    ends = []
+    pos = 0
+    for part in parts:
+        if part[1] >= 3:
+            # remember 0, 2, or more from either end is acceptable but not 1
+            beg = pos + part[1] - 3
+            begs.append(2 if beg == 1 else beg)
+            end = pos + 3
+            ends.append(len(str) - 2 if end == len(str) - 1 else end)
+        pos += part[1]
+    ends.append(pos) # at this point pos == len(str)
+    # find longest chunk
+    best_beg, best_end = max(((be[1] - be[0], be) for be in zip(begs, ends)), key=lambda x: x[0])[1]
+    # recurse because repeatedly finding the longest chunk minimizes the total number of chunks
+    return ssspookyyyy(str[:best_beg]) + [str[best_beg:best_end]] + ssspookyyyy(str[best_end:])
